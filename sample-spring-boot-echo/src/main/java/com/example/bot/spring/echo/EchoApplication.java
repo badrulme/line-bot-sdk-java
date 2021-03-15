0619360c2eeb3fe464c3a9cf9b36d38e8d9aa87d
Md. Badrul Alam <badrul.alam@bjitgroup.com>
2021-03-15 13:01:09 +0600

    Event check

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -47,6 +47,6 @@ public class EchoApplication {
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
+        System.out.println("event-all: " + event);
     }
 }
