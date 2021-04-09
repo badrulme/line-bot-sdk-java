5da2b87c62e2f7f246426980d9154032f77bd7cc
Badrul [11114] <badrul.alam@bjitgroup.com>
2021-04-09 16:03:09 +0600

    Test Controller added Log: Http Request

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/TestController.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/TestController.java
@@ -2,14 +2,24 @@ package com.example.bot.spring.echo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class TestController {
+    @Autowired
+    private final HttpServletRequest request;
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public TestController(HttpServletRequest request) {
+        this.request = request;
+    }
+
     @PostMapping("test")
     public void test(){
-        log.info("event");
+        log.info("event: "+request);
     }
 }
